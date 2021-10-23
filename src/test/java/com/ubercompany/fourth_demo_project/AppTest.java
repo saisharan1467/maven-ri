f24d3b599f5d0335bd28b6cd412132abb6504d11
saisharan1467 <gundarapusharanreddy@gmail.com>
2021-10-23 10:55:45 -0400

    adding project into github

---
+++ src/test/java/com/ubercompany/fourth_demo_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.ubercompany.fourth_demo_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
